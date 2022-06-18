b0a2d6921f4cc5cdeead9de5905e0c54e1a025d7
SebastianOberti <SEBASTIANOBERTI@OUTLOOK.COM>
2022-06-18 12:44:48 +0200

    update

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def prediction():
 # Ejecutamos la aplicación app.run()
 if __name__ == '__main__':
     # LOCAL
-    # app.run(host='0.0.0.0', port=8080)
+    #app.run(host='127.0.0.1', port=5000)
 
     # REMOTO
     app.run()
